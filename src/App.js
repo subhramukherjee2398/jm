08712b64507fa82a71ddd46c2d26ec5d7d30d929
Subhra Mukherjee <subhramukherjee560@gmail.com>
2022-10-15 22:54:03 +0530

    tailwind setup is done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react'
-
+import './App.css'
 const App = () => {
   return (
-    <div>App</div>
+    <h1 className="text-3xl font-bold underline">
+    Hello world!
+  </h1>
   )
 }
 
