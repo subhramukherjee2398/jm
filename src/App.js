2bd8dee2b943d01717dbd9d4e7ec8cbaf891085b
Subhra Mukherjee <subhramukherjee560@gmail.com>
2022-10-15 23:53:51 +0530

    all file setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import { TooltipComponent } from '@syncfusion/ej2-react-popups'
 import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
+import { GrSettingsOption } from 'react-icons/gr';
+
 import './App.css'
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+   <div>
+    <BrowserRouter>
+     <div className="flex relative dark:bg-main-dark-bg">
+     <div className="fixed right-4 bottom-4" style={{ zIndex: '1000' }}>
+       <TooltipComponent content="Settings" position="Top">
+        <button  className="text-3xl text-white p-3 hover:drop-shadow-xl  hover:rotate-45">
+          <GrSettingsOption />
+        </button>
+       </TooltipComponent>
+      </div>
+     </div>
+    </BrowserRouter>
+   </div>
   )
 }
 
