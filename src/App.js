d89d76fc74374713b34c1fc962c5b22885a4cd21
Subhra Mukherjee <subhramukherjee560@gmail.com>
2022-10-15 11:39:54 +0530

    file clearing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-    
-    </div>
-  );
+    <div>App</div>
+  )
 }
 
-export default App;
+export default App
