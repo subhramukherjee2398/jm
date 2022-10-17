58f4015dfc47f20e68c1c0803344698aad08defe
Subhra Mukherjee <subhramukherjee560@gmail.com>
2022-10-17 19:33:29 +0530

    sidebar start

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
 import { TooltipComponent } from '@syncfusion/ej2-react-popups'
-import React from 'react'
-import { BrowserRouter } from 'react-router-dom'
-import { GrSettingsOption } from 'react-icons/gr';
+import React, { useState } from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { FiSettings } from 'react-icons/fi';
 
 import './App.css'
+import { Sidebar } from './components';
 const App = () => {
+  const [activeMenu, setActivemenu] = useState(true)
   return (
-   <div>
-    <BrowserRouter>
-     <div className="flex relative dark:bg-main-dark-bg">
-     <div className="fixed right-4 bottom-4" style={{ zIndex: '1000' }}>
-       <TooltipComponent content="Settings" position="Top">
-        <button  className="text-3xl text-white p-3 hover:drop-shadow-xl  hover:rotate-45">
-          <GrSettingsOption />
-        </button>
-       </TooltipComponent>
-      </div>
-     </div>
-    </BrowserRouter>
-   </div>
+    <div>
+      <BrowserRouter>
+        <div className="flex relative dark:bg-main-dark-bg">
+          <div className="fixed right-4 bottom-4" style={{ zIndex: '1000' }}>
+            <TooltipComponent content="Settings" position="Top">
+              <button className="text-3xl text-white p-3 hover:drop-shadow-xl hover:bg-light-gray" style={{ background: 'blue', borderRadius: '50%' }}>
+                <FiSettings />
+              </button>
+            </TooltipComponent>
+          </div>
+          {activeMenu ?
+            <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white "><Sidebar /></div> : <div>nosdiebar</div>}
+
+          <div>
+            <div>Navbar</div>
+          </div>
+
+          <div>
+            <Routes>
+              {/**dashboard */}
+
+              <Route path='/' element={""} />
+              <Route path='/' element={""} />
+
+              {/**pages */}
+
+              <Route path='/' element={""} />
+              <Route path='/' element={""} />
+            </Routes>
+          </div>
+
+        </div>
+      </BrowserRouter>
+    </div>
   )
 }
 
---
+++ src/components/Sidebar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+import { SiShopware } from "react-icons/si";
+import { MdOutlineCancel } from "react-icons/md";
+
+const Sidebar = () => {
+  return (
+    <div className="ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10" style={{backgroundColor:'red'}}>
+        sidbar
+    </div>
+  );
+};
+
+export default Sidebar;
