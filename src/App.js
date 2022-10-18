c284c1f75a80f7ee16728a939cb163d9b8bf8cd8
Subhra Mukherjee <subhramukherjee560@gmail.com>
2022-10-18 21:42:28 +0530

    earning dashboard done

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { FiSettings } from 'react-icons/fi';
 
 import './App.css'
-import { Sidebar,Navbar } from './components';
+import { Sidebar, Navbar } from './components';
 import { useStateContext } from './contexts/ContextProvider';
+import Ecommerce from './pages/Ecommerce';
 
 const App = () => {
-  
-  const {  activeMenu } = useStateContext();
+
+  const { activeMenu } = useStateContext();
 
   return (
     <div>
@@ -23,24 +24,24 @@ const App = () => {
             </TooltipComponent>
           </div>
           {activeMenu ?
-            <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white "><Sidebar /></div> :<div className="w-0 dark:bg-secondary-dark-bg">
-            <Sidebar />
-          </div>}
+            <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white "><Sidebar /></div> : <div className="w-0 dark:bg-secondary-dark-bg">
+              <Sidebar />
+            </div>}
 
           <div className={
-              activeMenu
-                ? 'dark:bg-main-dark-bg  bg-main-bg min-h-screen md:ml-72 w-full  '
-                : 'bg-main-bg dark:bg-main-dark-bg  w-full min-h-screen flex-2 '
-            }>
-            <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full "><Navbar/></div>
-          </div>
+            activeMenu
+              ? 'dark:bg-main-dark-bg  bg-main-bg min-h-screen md:ml-72 w-full  '
+              : 'bg-main-bg dark:bg-main-dark-bg  w-full min-h-screen flex-2 '
+          }>
+            <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full "><Navbar /></div>
+         
 
           <div>
             <Routes>
               {/**dashboard */}
 
-              <Route path='/' element={""} />
-              <Route path='/' element={""} />
+              <Route path="/" element={(<Ecommerce />)} />
+              <Route path="/ecommerce" element={(<Ecommerce />)} />
 
               {/**pages */}
 
@@ -48,7 +49,7 @@ const App = () => {
               <Route path='/' element={""} />
             </Routes>
           </div>
-
+          </div>
         </div>
       </BrowserRouter>
     </div>
---
+++ src/pages/Ecommerce.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import { BsCurrencyDollar } from 'react-icons/bs';
+import Button from '../components/Button';
+import { earningData, medicalproBranding, recentTransactions, weeklyStats, dropdownData, SparklineAreaData, ecomPieChartData } from '../data/dummy';
+const Ecommerce = () => {
+    return (
+        <div className="mt-24">
+            <div className="flex flex-wrap lg:flex-nowrap justify-center">
+                {/** 1st tab */}
+                <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl w-full lg:w-80 p-8 pt-9 m-3 bg-hero-pattern bg-no-repeat bg-cover bg-center first-tab-design illustration-img">
+                    <div className="flex justify-between items-center ">
+                        <div>
+                            <p className="font-bold text-gray-400">Earnings</p>
+                            <p className="text-2xl">$63,448.78</p>
+                        </div>
+                        <button type="button"
+                            style={{ backgroundColor: 'blue' }}
+                            className="text-2xl opacity-0.9 text-white hover:drop-shadow-xl rounded-full  p-4">
+                            <BsCurrencyDollar />
+                        </button>
+                    </div>
+                    <div className="mt-6">
+                        <Button
+                            color="white"
+                            bgColor='blue'
+                            text="Download"
+                            borderRadius="10px"
+                        />
+                    </div>
+                </div>
+                <div className="flex m-3 flex-wrap justify-center gap-2 items-center">
+                    {earningData.map((item) => (
+                        <div key={item.title} className="bg-white h-44 dark:text-gray-200 dark:bg-secondary-dark-bg md:w-56  p-4 pt-9 rounded-2xl ">
+                            <button
+                                type="button"
+                                style={{ color: item.iconColor, backgroundColor: item.iconBg }}
+                                className="text-2xl opacity-0.9 rounded-full  p-4 hover:drop-shadow-xl"
+                            >
+                                {item.icon}
+                            </button>
+                            <p className="mt-3">
+                                <span className="text-lg font-semibold">{item.amount}</span>
+                                <span className={`text-sm text-${item.pcColor} ml-2`}>
+                                    {item.percentage}
+                                </span>
+                            </p>
+                            <p className="text-sm text-gray-400  mt-1">{item.title}</p>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Ecommerce;
