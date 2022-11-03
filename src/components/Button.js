ca97a2e1681db6de423566a0d0850c2e93799c93
Subhra Mukherjee <subhramukherjee560@gmail.com>
2022-11-03 18:37:52 +0530

    navbar is fully complete

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,12 +3,12 @@ import React from 'react';
 import { useStateContext } from '../contexts/ContextProvider';
 
 const Button = ({ icon, bgColor, color, bgHoverColor, size, text, borderRadius, width }) => {
-    const { setIsClicked, initialState } = useStateContext();
+    const { setIsClick, initialState } = useStateContext();
 
     return (
         <button
             type="button"
-            onClick={() => setIsClicked(initialState)}
+            onClick={() => setIsClick(initialState)}
             style={{ backgroundColor: bgColor, color, borderRadius }}
             className={` text-${size} p-3 w-${width} hover:drop-shadow-xl hover:bg-${bgHoverColor}`}
         >
--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -11,16 +11,16 @@ const initialState = {
 
 export const ContextProvider = ({ children }) => {
   const [activeMenu, setActiveMenu] = useState(true);
-  const [isClicked,setIsClick] = useState(initialState);
+  const [isClicked, setIsClick] = useState(initialState);
   const [screenSize, setScreenSize] = useState(undefined);
   const [currentColor, setCurrentColor] = useState('#03C9D7');
   const [currentMode, setCurrentMode] = useState('Light');
   const [themeSettings, setThemeSettings] = useState(false);
 
 
- const handleClick = (click) =>{
-   console.log(click)
-   setIsClick({...initialState,[click]:true})
+  const handleClick = (click) => {
+    console.log(click)
+    setIsClick({ ...initialState, [click]: true })
   }
 
   const setMode = (e) => {
@@ -35,7 +35,7 @@ export const ContextProvider = ({ children }) => {
 
 
   return (
-    <StateContext.Provider value={{ activeMenu, setActiveMenu,isClicked,handleClick,screenSize,setScreenSize,currentColor,currentMode,setColor,setMode,themeSettings,setThemeSettings}}>
+    <StateContext.Provider value={{ activeMenu, setActiveMenu, isClicked, handleClick, screenSize, setScreenSize, currentColor, currentMode, setColor, setMode, themeSettings, setThemeSettings, setIsClick, initialState }}>
       {children}
     </StateContext.Provider>
   );
